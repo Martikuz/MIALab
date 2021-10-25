f01196160fdea38a705449d3d4dd138038135398
Martikuz <martinkunz@gmail.com>
2021-10-25 13:30:30 +0200

    ref(pipeline_utilities): shortened import on HausdorffDistance

--- mialab/utilities/pipeline_utilities.py
+++ mialab/utilities/pipeline_utilities.py
@@ -295,7 +295,7 @@ def init_evaluator() -> eval_.Evaluator:
 
     # initialize metrics
     # todo: add hausdorff distance, 95th percentile (see metric.HausdorffDistance)
-    metrics = [metric.DiceCoefficient(), metric.categorical.HausdorffDistance(percentile=95)]
+    metrics = [metric.DiceCoefficient(), metric.HausdorffDistance(percentile=95)]
     #warnings.warn('Initialized evaluation with the Dice coefficient. Do you know other suitable metrics?')
 
     # define the labels to evaluate
